9305e8be1a0f02bb06c188817680b3ad0e2d5dd5
rn0711 <rena_255@icloud.com>
2022-01-18 18:23:57 +0900

    モデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    first_name {'山田'}
+    last_name {'隆太郎'}
+    first_name_kana {'ヤマダ'}
+    last_name_kana {'リクタロウ'}
+    date_of_birth {Faker::Date.birthday}
+  end
+end
