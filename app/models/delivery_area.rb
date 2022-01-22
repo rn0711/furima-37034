7eb42f24e6f454be6b3e7bb0e276eb1e4e3a7de8
rn0711 <rena_255@icloud.com>
2022-01-22 17:40:13 +0900

    モデルとビューファイルの修正

--- app/models/delivery_area.rb
+++ app/models/delivery_area.rb
@@ -1,4 +1,4 @@
-class Delivery_area < ActiveHash::Base
+class DeliveryArea < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '北海道' },
@@ -37,16 +37,17 @@ class Delivery_area < ActiveHash::Base
     { id: 35, name: '広島県' },
     { id: 36, name: '山口県' },
     { id: 37, name: '徳島県' },
-    { id: 38, name: '愛媛県' },
-    { id: 39, name: '高知県' },
-    { id: 40, name: '福岡県' },
-    { id: 41, name: '佐賀県' },
-    { id: 42, name: '長崎県' },
-    { id: 43, name: '熊本県' },
-    { id: 44, name: '大分県' },
-    { id: 45, name: '宮崎県' },
-    { id: 46, name: '鹿児島県' },
-    { id: 47, name: '沖縄県' }
+    { id: 38, name: '香川県' },
+    { id: 39, name: '愛媛県' },
+    { id: 40, name: '高知県' },
+    { id: 41, name: '福岡県' },
+    { id: 42, name: '佐賀県' },
+    { id: 43, name: '長崎県' },
+    { id: 44, name: '熊本県' },
+    { id: 45, name: '大分県' },
+    { id: 46, name: '宮崎県' },
+    { id: 47, name: '鹿児島県' },
+    { id: 48, name: '沖縄県' }
   ]
 
   include ActiveHash::Associations
