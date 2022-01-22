92df9efbd9e3e4d6d7ca474f458fb38c5529ddd4
rn0711 <rena_255@icloud.com>
2022-01-22 11:46:04 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_area
+  belongs_to :delivery_date
+  belongs_to :postage
+
+  validates :image, :itemname, :introduction, :price, presence: true
+
+  validates :category_id, :condition_id, :postage_id, :delivery_area_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
