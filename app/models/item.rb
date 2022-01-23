55b0f69c2d6ce57d7bd828630e3e553799ba4cbe
rn0711 <rena_255@icloud.com>
2022-01-23 12:22:38 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   validates :category_id, :condition_id, :postage_id, :delivery_area_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank"}
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
 
